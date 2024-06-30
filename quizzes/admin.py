becd59351c58cc4188ac899f13c6c7b568d9815b
milkawinoh <milkawinoh@gmail.com>
2024-06-30 06:50:34 +0300

    configured admin site for models.only registerd it.

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Quiz, Question, Choice, Result
+class Choiceinline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [Choiceinline]
+
+admin.site.register(Quiz)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Result)
